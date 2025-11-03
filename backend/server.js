6abd22f9c9b3c05b1f97d7928bda28211136e777
AMJAMAITHILI <amjamaithili@gmail.com>
2025-11-03 20:24:16 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://projectvault-cbit.onrender.com" }));
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
